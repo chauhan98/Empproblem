c63c3aa5f37a0b0a5610679bb1388e72c74a8531
chauhan98 <cshashank.2598@gmail.com>
2021-10-26 10:38:12 +0530

    Completed Uc1: checking if employee is present or absent

--- src/employeeproblem/EmployeeProblem.java
+++ src/employeeproblem/EmployeeProblem.java
@@ -1,7 +1,17 @@
 package employeeproblem;
 
+import java.util.Random;
+
 public class EmployeeProblem {
     public static void main(String[] args) {
         System.out.println("Welcome To Employee Wage Computation Problem");
+        // to check if Employee is present or absent
+        Random random = new Random();
+        //define variable
+        int number = random.nextInt(2);
+        if (number == 1)
+            System.out.println("Employee is present");
+        else
+            System.out.println("Employee is absent");
     }
 }
