488f9a80e6e025c6eefcf2dd0582e4f60d695444
chauhan98 <cshashank.2598@gmail.com>
2021-10-26 10:56:55 +0530

    Calculating Employee Wage

--- src/employeeproblem/EmployeeProblem.java
+++ src/employeeproblem/EmployeeProblem.java
@@ -4,14 +4,22 @@ import java.util.Random;
 
 public class EmployeeProblem {
     public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Problem");
-        // to check if Employee is present or absent
+        System.out.println("Welcome to Employee Wage problem");
+        //generating random number
         Random random = new Random();
-        //define variable
+        //variableS
+        int WAGE_PER_HOUR = 20;
+        int TOTAL_NO_OF_HOUR = 8;
         int number = random.nextInt(2);
-        if (number == 1)
+        //checking if emp is present or absent
+        if (number == 1) {
             System.out.println("Employee is present");
-        else
-            System.out.println("Employee is absent");
+            //computation of emp wage
+            System.out.println("DailyWage = " + WAGE_PER_HOUR * TOTAL_NO_OF_HOUR);
+        }
+                else
+                System.out.println("Employee is absent");
     }
 }
+
+
