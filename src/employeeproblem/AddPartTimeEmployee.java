84b96c26b12922873a496dc8ba67aae95c0e39cf
chauhan98 <cshashank.2598@gmail.com>
2021-10-26 11:04:00 +0530

    Adding Part Time Employee And Calculating Wage

---
+++ src/employeeproblem/AddPartTimeEmployee.java
@@ -0,0 +1,32 @@
+package employeeproblem;
+
+public class AddPartTimeEmployee {
+
+        // initialize variable and assign value
+        int is_Full_Time = 1;
+        int is_Part_Time = 2;
+        int Emp_Rate_Per_Hour = 20;
+        int emp_Hour = 0;
+        public void calculateWage() {
+            // take the random value
+            int checkEmp = (int) Math.floor(Math.random() * 10) % 3;
+            // check the condition emp is present or not
+            if(checkEmp==is_Full_Time)
+            {
+                emp_Hour = 8;
+                System.out.println("Employee is Full time");
+            }
+            // part time 2
+            else if(checkEmp==is_Part_Time)
+            {
+                emp_Hour = 4;
+                System.out.println("Employee is Part Time");
+            }
+            else System.out.println("Emplyee is Absent");
+            // calculate emp wage
+            int empWage = emp_Hour * Emp_Rate_Per_Hour;
+            // print emp wage
+            System.out.println("Employee Wage is : "+empWage);
+        }
+    }
+
--- src/employeeproblem/EmployeeProblem.java
+++ src/employeeproblem/EmployeeProblem.java
@@ -1,24 +1,13 @@
 package employeeproblem;
 
-import java.util.Random;
+
 
 public class EmployeeProblem {
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee Wage problem");
-        //generating random number
-        Random random = new Random();
-        //variableS
-        int WAGE_PER_HOUR = 20;
-        int TOTAL_NO_OF_HOUR = 8;
-        int number = random.nextInt(2);
-        //checking if emp is present or absent
-        if (number == 1) {
-            System.out.println("Employee is present");
-            //computation of emp wage
-            System.out.println("DailyWage = " + WAGE_PER_HOUR * TOTAL_NO_OF_HOUR);
-        }
-                else
-                System.out.println("Employee is absent");
+
+        AddPartTimeEmployee emp1 = new AddPartTimeEmployee();
+        emp1.calculateWage();
+
     }
 }
 
