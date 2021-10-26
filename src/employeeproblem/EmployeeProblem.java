31bf38d9a24814091f8128b93ff19e804d1a9b57
chauhan98 <cshashank.2598@gmail.com>
2021-10-26 10:32:18 +0530

    Welcome to Employee Wage Computation Problem

---
+++ src/employeeproblem/EmployeeProblem.java
@@ -0,0 +1,7 @@
+package employeeproblem;
+
+public class EmployeeProblem {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Problem");
+    }
+}
